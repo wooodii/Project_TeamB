d331ff0c988c2917ba9e0ad25cc5ec1f47e19338
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2022-12-21 17:32:48 +0900

    12/21 filterCSS

--- src/data/categorydata.js
+++ src/data/categorydata.js
@@ -2,121 +2,121 @@
 
     const categorydata = [
         {
-            icon : "../img/mydoctor.png",
+            icon : 'major0.png' ,
             id : 0,
             name : '내과', 
         },
 
         {
-            icon : '',
+            icon : 'major1.png',
             id : 1,
             name : '소아청소년과'
         }, 
 
         {
-            icon : '',
+            icon : 'major2.png',
             id : 2,
             name : '피부과' 
         }, 
 
         {
-            icon : '',
+            icon : 'major3.png',
             id : 3,
             name : '정형외과'
         },
 
         {
-            icon : '',
+            icon : 'major4.png',
             id : 4,
             name : '치과'
         },
 
         {
-            icon : '',
+            icon : 'major5.png',
             id : 5,
             name : '가정의학과'
         },
 
         {
-            icon : '',
+            icon : 'major6.png',
             id : 6,
             name : '이비인후과'
         },
 
         {
-            icon : '',
+            icon : 'major7.png',
             id : 7,
             name : '비뇨의학과'
         },
 
         {
-            icon : '',
+            icon : 'major8.png',
             id : 8,
             name : '산부인과'
         },
 
         {
-            icon : '',
+            icon : 'major9.png',
             id : 9,
             name : '안과'
         },
 
         {
-            icon : '',
+            icon : 'major10.png',
             id : 10,
             name : '외과'
         },
 
         {
-            icon : '',
+            icon : 'major11.png',
             id : 11,
             name : '신경과'
         },
 
         {
-            icon : '',
+            icon : 'major12.png',
             id : 12,
             name : '정신건강의학과'
         },
 
         {
-            icon : '',
+            icon : 'major13.png',
             id : 13,
             name : '성형외과'
         },
 
         {
-            icon : '',
+            icon : 'major14.png',
             id : 14,
             name : '한방내과'
         },
 
         {
-            icon : '',
+            icon : 'major15.png',
             id : 15,
             name : '흉부외과'
         },
 
         {
-            icon : '',
+            icon : 'major16.png',
             id : 16,
             name : '신경외과'
         },
 
         {
-            icon : '',
+            icon : 'major17.png',
             id : 17,
             name : '재활의학과'
         },
 
         {
-            icon : '',
+            icon : 'major18.png',
             id : 18,
             name : '마취통증의학과'
         },
 
         {
-            icon : '',
+            icon : 'major19.png',
             id : 19,
             name : '영상의학과'
         },
