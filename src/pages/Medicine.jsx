27f70365079d770bbff4e72f7c333931c9cae56f
sophy97 <112137303+sophy97@users.noreply.github.com>
2022-12-23 23:33:34 +0900

    복약관리 완료

--- src/pages/Medicine.jsx
+++ src/pages/Medicine.jsx
@@ -1,22 +1,36 @@
 /** 복약관리 페이지입니다 (서아)
- * 로그인한 유저 > 유저정보와 연결해서 직접 약 등록과 수정, 삭제 기능만 (리덕스)
- * firebase db에는 저장 안 함
- */
-
+*로그인한 유저 > 유저정보와 연결해서 직접 약 이름과 투여량, 횟수를 입력 > 출력하는 페이지
+*상태관리 리덕스(medicine.js)/firebase db에는 저장 안 함
+*/
 import { useContext, useEffect, useState } from "react";
-import { FloatingLabel, Form, Button, Card, ListGroup } from "react-bootstrap";
+import { FloatingLabel, Form, Card, ListGroup, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addMedicine } from "../modules/medicine";
 import DataContext from "../data/DataContext";
 import { doc, getDoc } from "firebase/firestore"
 import { db } from "../Firebase";
+import "../css/Medicine.css";
 
 const Medicine = () => {
     const data = useContext(DataContext);
     const user = localStorage.getItem("currentUser");
     const [name, setName] = useState();
-    const [pills, setPills] = useState("");
+    const [pillsName, setPillsName] = useState("");
+    const [pillsCount, setpillsCount] = useState(0);
+    const [pillsDose, setPillsDose] = useState(0);
+    // +리덕스로 medicine.js 값 가져와서 map으로 출력
+    const medicine = useSelector((state)=>(state.medicine));
+    const dispatch = useDispatch();
 
+    // 복약정보 입력 모달창 관리 state입니다
+    const [modalOpen, setModalOpen] = useState(false);
+    // 복약정보 입력하기 버튼에 연결된 함수
+    function openMedModal () {
+        return (
+            setModalOpen(!modalOpen)
+        )
+    }
+    // firestore db에서 로그인된 유저 정보를 들고온다
     const getSingleData = async () => {
         const docRef = doc(db, "users", user);
         const docSnap = await getDoc(docRef);
@@ -24,9 +38,6 @@ const Medicine = () => {
         setName(docSnap.data().name);
         }
     }
-    // +리덕스로 medicine.js 값 가져와서 map으로 출력
-    const medicine = useSelector((state)=>(state.medicine));
-    const dispatch = useDispatch();
 
     useEffect(() => {
         if (user) {
@@ -36,59 +47,89 @@ const Medicine = () => {
             data.action.setIsLoginned(false)
         }
     }, [user]);
-    //console.log(pills);
 
     return ( 
-        <div className="mx-5 mt-5">
-                    <>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="이름"
-                        className="mb-3">
-                        <Form.Control type="text" value={name} 
-                        style={{border:"none", borderBottom:"1px solid lightgray"}}
-                        onChange={(e)=>{setName(e.target.value)}} />
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingTextarea2" label="복용하는 약을 작성해주세요">
-                        <Form.Control
-                        as="textarea" onChange={(e)=>{setPills(e.target.value)}}
-                        style={{ height: '100px' }} />
-                    </FloatingLabel>
-                    </>
-            <br />
-            <Button variant="outline-primary"
-            onClick={()=>{dispatch(addMedicine({ name: name, pills: pills }));}}
-            >작성</Button>
-            <hr />
-            <h4>복용하는 약</h4> <br />
-            <Card style={{ width:"80%" }}>
-                <ListGroup variant="flush">
-                    {
-                        medicine.map((medicine)=> (
-                        <div key={medicine.pillId}>
-                            <PrintMedicine medicine={medicine} />
-                        </div> 
-                        ))
-                    }
-                </ListGroup>
-            </Card>
+        <>
+        <div className="med-form">
+            <h3>복약 관리</h3>
+            <p>버튼을 클릭하여 복약정보를 등록하세요!</p>
         </div>
+        <button className="addMed-btn med-form" onClick={openMedModal}>복약정보 추가</button>
+        <br />
+        {modalOpen ? (
+        <MedicineModal 
+        name={name} pillsCount={pillsCount} setpillsCount={setpillsCount}
+        pillsDose={pillsDose} setPillsDose={setPillsDose}
+        pillsName={pillsName} setPillsName={setPillsName} 
+        modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        ):(null)
+        }
+        <br />
+        <div className="print-medList med-form">
+        <b>{name}</b>{" "}님의 복약정보
+        <Table striped>
+                <thead>
+                    <tr>
+                    <th>no.</th>
+                    <th>이름</th>
+                    <th><p><b>투여량</b></p></th>
+                    <th><p><b>투여 횟수</b></p></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {medicine.map((med)=>(
+                    <tr key={med.pillId}>
+                        <td>{med.pillId}</td>
+                        <td>{med.pillsName}</td>
+                        <td>{med.pillsCount}</td>
+                        <td>{med.pillsDose}</td>
+                    </tr>
+                    ))}  
+                </tbody>
+        </Table>
+        </div>
+        </>
     );
 }
 
 export default Medicine;
 
 
+
 // +추가
-// 복약관리 내역을 출력할 공간 ui - 컴포넌트로 작성
-// react-bootstrap 의 ListGroup.Item에 출력하고자 함
-const PrintMedicine =({medicine})=> {
+// 복약 정보 등록전, 약 이름, 투여 수량과 횟수를 입력받는 모달창입니다
+// 상위 컴포넌트로부터 값을 넘겨받아 출력하기 위한 props들
+const MedicineModal =(props)=> {
+    const {name, pillsName, setPillsName, pillsCount, setpillsCount, pillsDose, setPillsDose} = props;
+    const {modalOpen, setModalOpen} = props;
+    //리덕스에서 값 가져오기 위함
+    const dispatch = useDispatch();
     return (
-        <ListGroup.Item>
-            <b>{medicine.name}</b>
+        <>
+        <div className="med-form">
+            <p>
+            <b>{name}</b>{" "}님, <br/> 복용중인 약의 이름과 투여량, 횟수를 입력해주세요</p>
+        </div>
+        <hr/>
+        <div className="mx-5 mt-5">
+            <div>
+                <FloatingLabel controlId="floatingInput"
+                    label="복용하는 약의 이름" className="mb-3">
+                <Form.Control type="text" 
+                    style={{border:"none", borderBottom:"1px solid lightgray"}}
+                    onChange={(e)=>{setPillsName(e.target.value)}} />
+                </FloatingLabel>
+                투여량: <input className="med-input" type="number" onChange={(e)=>{setpillsCount(e.target.value)}} />
+                투여 횟수: <input className="med-input" type="number" min='10' max='30' onChange={(e)=>{setPillsDose(e.target.value)}} />
+            </div>
             <br />
-            {medicine.pills}
-        </ListGroup.Item>
+                <button className="addMed-btn"
+                onClick={()=>{dispatch(addMedicine(
+                    { pillsName: pillsName , pillsCount:pillsCount, pillsDose:pillsDose }
+                    )); setModalOpen(!modalOpen); alert("등록 완료"); 
+                }} >등록하기</button>
+        </div>
+    </>
     )
 }
 
