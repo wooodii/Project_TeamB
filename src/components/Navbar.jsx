0952a892f03dc88cbce6ec3b18230836e49ecc8f
wooodii <jihyeonkim1@naver.com>
2022-12-28 16:31:08 +0900

    1228 nav, home 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,22 +10,28 @@ import { faList, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { Col, Container, Row } from "react-bootstrap";
 
 const NavbarComp = () => {
-
     const navigate = useNavigate();
+    const colorchange = () => {
+        
+    };
 
     return (
 
         <div className="footer">
         <Container>
-        <Row>
-            <Col onClick={()=>{navigate('/home')}}><FontAwesomeIcon icon={faHouse} size="2x" /></Col>
-            <Col onClick={()=>{navigate('/history')}}><FontAwesomeIcon icon={faList} size="2x" /></Col>
-            <Col button onClick={()=>{navigate('/mypage')}}><FontAwesomeIcon icon={faUser} size="2x" /></Col>
-        </Row>
-        <Row>
-            <Col><button onClick={()=>{navigate('/home')}}>홈</button></Col>
-            <Col><button onClick={()=>{navigate('/history')}}>예약내역</button></Col>
-            <Col><button onClick={()=>{navigate('/mypage')}}>마이페이지</button></Col>
+        <Row style={{backgroundColor : "#1b4542", padding : "1em"}}>
+            <Col onClick={()=>{navigate('/history')}}>
+                    <FontAwesomeIcon icon={faList} style={{color : "white", width : "0.8em"}} size="2x" />
+                {/* <button style={{color : "white", fontSize : "0.8em"}} onClick={()=>{navigate('/history')}}>예약내역</button> */}
+            </Col>
+            <Col onClick={()=>{navigate('/home');}}>
+                <FontAwesomeIcon icon={faHouse} style={{color : "white", width : "0.8em"}} size="2x" /> 
+                {/* <button style={{color : "white"}} onClick={()=>{navigate('/home')}}>홈</button> */}
+            </Col>
+            <Col button onClick={()=>{navigate('/mypage')}}>
+                <FontAwesomeIcon icon={faUser} style={{color : "white", width : "0.8em"}} size="2x" />
+                {/* <button style={{color : "white"}} onClick={()=>{navigate('/mypage')}}>마이페이지</button> */}
+            </Col>
         </Row>
         </Container>
 
